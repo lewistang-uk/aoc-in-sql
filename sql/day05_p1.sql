3d4a5fcedb0ac39dc44a74315100619140cd6b3a
lewistang-uk <ltang302@gmail.com>
2026-08-10 09:13:26 +0100

    solved day 5

---
+++ sql/day05_p1.sql
@@ -0,0 +1,29 @@
+-- day one
+
+-- get ranges
+WITH parsed_ranges AS (
+    SELECT
+        CAST(substr(ln, 1, instr(ln, '-')-1) AS INTEGER) AS range_start,
+        CAST(substr(ln, instr(ln, '-')+1) AS INTEGER) AS range_end
+    FROM day5
+    WHERE ln LIKE '%-%'
+),
+
+-- get numbers
+parsed_numbers AS (
+    SELECT CAST(ln AS INTEGER) AS num
+    FROM day5
+    WHERE ln NOT LIKE '%-%'
+    AND ln <> ''
+)
+
+-- check if number appears in any range
+SELECT COUNT(*)
+FROM parsed_numbers
+WHERE num IN (
+    SELECT n.num
+    FROM parsed_numbers n
+    CROSS JOIN parsed_ranges r
+    ON r.range_start <= n.num
+    AND n.num <= r.range_end
+);
---
+++ sql/day05_p2.sql
@@ -0,0 +1,42 @@
+-- day two
+
+-- find ranges
+WITH parsed_ranges AS (
+    SELECT
+        CAST(substr(ln, 1, instr(ln, '-')-1) AS INTEGER) AS range_start,
+        CAST(substr(ln, instr(ln, '-')+1) AS INTEGER) AS range_end
+    FROM day5
+    WHERE ln LIKE '%-%'
+),
+
+-- implement Klee's algorithm
+
+-- define start and stop events
+events AS (
+    SELECT
+        range_start AS ts,
+        1 AS flag
+    FROM parsed_ranges
+
+    UNION ALL
+
+    SELECT
+        range_end + 1 AS ts, -- integer problem so a range [a, b] converted to [a, b+1)
+        -1 AS flag
+    FROM parsed_ranges
+),
+
+-- find the sum of flags
+cumsum AS (
+    SELECT
+        ts,
+        LEAD(ts) OVER (ORDER BY ts, flag) AS lead_ts,
+        SUM(flag) OVER (ORDER BY ts, flag) AS active
+        FROM events
+)
+
+-- sum the differences between events when valid
+SELECT SUM(lead_ts - ts) 
+FROM cumsum
+WHERE active > 0
+;
